a862aeb6f5c724a90627bb1dfc3f29f1f4ab8867
Bally <bkss.designs1@gmail.com>
2024-01-22 19:54:56 +0000

    Created Gate class, admin.txt now created

---
+++ src/GardenGateProblem.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class GardenGateProblem {
+
+  static public void main(String[] args) {
+    RandomAccessFile admin;
+    byte init[] = {0};
+
+    if (args.length != 1)
+      System.err.println("usage: java {gate_bottom,gate_top}");
+    else {
+      if(args[0].compareToIgnoreCase("gate_bottom") ==0) {
+         System.out.println("running the bottom gate");
+         try{
+          admin = new RandomAccessFile("admin.txt","rw");
+          admin.seek(0);
+          admin.write(init);
+          admin.close();
+         }
+         catch (IOException e){
+            System.out.println("something wrong with file access" + e);
+         }
+      } else
+         System.out.println("running the top gate");
+    //  Gate counter = new Gate();
+    //  counter.counting();
+    }
+  }
+}
