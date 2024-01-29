9ea33ed3ec11e5464ad07512a638a2ed4f3f17e4
Bally <bkss.designs1@gmail.com>
2024-01-29 20:29:52 +0000

    Implemented overwrite protection

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-Week 0 - OS Concurrancy & Distribution Practical
+Lab 0 - OS Concurrancy & Distribution Practical
+Lab 1 - Implemented FileLock for overwrite protection
--- src/Gate.java
+++ src/Gate.java
@@ -1,11 +1,13 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
 
 public class Gate {
 
     final private int MAX_PEOPLE = 50;
     private RandomAccessFile file;
+    private FileLock lock;
 
 
     public Gate() throws FileNotFoundException {
@@ -16,33 +18,30 @@ public class Gate {
         byte[] init = {0};
 
         for (int i = 0; i < MAX_PEOPLE; i++) {
-            System.out.println("Internal counter: " + (i+1));
+            System.out.println("Gate counter: " + (i+1));
 
             try {
-
+                lock = file.getChannel().lock(); // enable the file lock for overwrite protection
                 file.seek(0);
                 int count = file.read();
 
                 System.out.println("File counter: " + count);
                 count += 1;
-
                 Thread.sleep(200);
-
                 file.seek(0);
                 file.write((byte) count);
+                lock.release(); // release the file lock so other processes can write to the file
 
 
 
-
-                if (i == MAX_PEOPLE){
+                if (i == (MAX_PEOPLE-1)){
                     file.close();
                 }
             }
-            catch (InterruptedException e) {
-                System.out.println("Something went wrong");
-            }
             catch (IOException e) {
                 System.out.println("File access error");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
