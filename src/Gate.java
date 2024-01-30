d7a3587c0621a5a7cf575c0927c2874350f8a97f
Bally <bkss.designs1@gmail.com>
2024-01-30 16:56:17 +0000

    Added check for each gate to compare if the opposite counter is bigger than the current counter of the gate

--- src/Gate.java
+++ src/Gate.java
@@ -20,26 +20,60 @@ public class Gate {
             try {
                 int count = 0;
                 if (gate.equals("gate_bottom")){
+
+                    // Check the counter of the bottom gate
                     file.seek(0);
                     count = file.read();
-                    System.out.println("File counter: " + count);
+                    System.out.println("Bottom file counter: " + count);
 
+                    // Increment the bottom gate counter
                     count += 1;
                     Thread.sleep(200);
+
+                    // Check the counter of top gate
+                    file.seek(1);
+                    int getTopCounter = file.read();
+                    System.out.println("getTopCounter: " + getTopCounter);
+
+                    // Has the counter advanced since the last iteration?
+                    if (getTopCounter > count){
+                        // Set the value of count to be getBottomCounter and increment it
+                        count = (getTopCounter + 1);
+                    }
+
+                    // Write the bottom gate counter to file
                     file.seek(0);
                 }
                 else if(gate.equals("gate_top")){
+
+                    // Check the counter of the top gate
                     file.seek(1);
                     count = file.read();
-                    System.out.println("File counter: " + count);
+                    System.out.println("Top file counter: " + count);
 
+                    // Increment the top gate counter
                     count += 1;
                     Thread.sleep(200);
+
+                    // Check the counter of the bottom gate
+                    file.seek(0);
+                    int getBottomCounter = file.read();
+                    System.out.println("getBottomCounter: " + getBottomCounter);
+
+                    // Has the counter advanced since the last iteration?
+                    if (getBottomCounter > count){
+                        // Set the value of count to be getBottomCounter and increment it
+                        count = (getBottomCounter + 1);
+                    }
+
+                    // Write the top gate counter to file
                     file.seek(1);
                 }
 
+
                 file.write((byte) count);
 
+                // Close file
                 if (i == (MAX_PEOPLE-1)){
                     file.close();
                 }
